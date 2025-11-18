557c12f17aa85acc85f4d12b40547fb1ba144c9d
Lucas <AbbyNode@users.noreply.github.com>
2025-11-17 22:50:29 -0500

    stop grace period

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ set -e
 
 echo ""
 echo "=== Minecraft Modpack Docker - Setup & Initialization ==="
-echo "Script version: 2025-11-17.9"
+echo "Script version: 2025-11-17.10"
 
 
 # ========== Update ==========
